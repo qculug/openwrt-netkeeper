921b0b543aeb6cf72d0ac3b26899c8b711708aaf
Roald Clark <roaldclark@gmail.com>
2022-03-14 14:06:58 +0800

    feat: enable_rp_pppoe_server to change_ppp_sh

--- files/netkeeper-init.sh
+++ files/netkeeper-init.sh
@@ -12,9 +12,19 @@ change_ppp_options() {
     echo "show-password" >> /etc/ppp/options
 }
 
+change_ppp_sh() {
+    if [ -f '/lib/netifd/proto/ppp.sh.bak' ]; then
+        echo "info: You may have executed this command."
+        exit 0
+    fi
+    cp /lib/netifd/proto/ppp.sh /lib/netifd/proto/ppp.sh.bak
+    sed -i "/proto_run_command/i __username=\$(echo -e \"\$username\")" /lib/netifd/proto/ppp.sh
+    sed -i 's/__username=/\tusername=/' /lib/netifd/proto/ppp.sh
+}
+
 install_rp_pppoe_so() {
     if [ -f '/etc/ppp/plugins/rp-pppoe.so' ]; then
-        echo "info: rp-pppoe.so is loaded."
+        echo "info: rp-pppoe.so is loaded, you may have executed this command."
         exit 0
     fi
     PPPD_VERSION="$(ls /usr/lib/pppd)"
@@ -22,6 +32,10 @@ install_rp_pppoe_so() {
 }
 
 add_network_netkeeper() {
+    if [ -n "$(uci get network.netkeeper 2> /dev/null)" ]; then
+        echo "info: network.netkeeper has been added, you may have executed this command."
+        exit 0
+    fi
     uci set network.netkeeper=interface
     uci set network.netkeeper.device="$(uci get network.wan.device)"
     uci set network.netkeeper.proto='pppoe'
@@ -33,34 +47,28 @@ add_network_netkeeper() {
 }
 
 set_firewall_for_netkeeper() {
+    if [ -n "$(uci get firewall.@zone[1].network | grep netkeeper)" ]; then
+        echo "info: netkeeper has been added to the firewall, you may have executed this command."
+        exit 0
+    fi
     uci set firewall.@zone[1].network="$(uci get firewall.@zone[1].network) netkeeper"
     uci commit firewall
 }
 
 servers_restart() {
-    /etc/init.d/firewall restart
     /etc/init.d/network reload
-    /etc/init.d/network restart
-}
-
-enable_rp_pppoe_server() {
-    if [ -f '/lib/netifd/proto/ppp.sh.bak' ]; then
-        echo "info: You may have executed this command."
-        exit 0
-    fi
-    cp /lib/netifd/proto/ppp.sh /lib/netifd/proto/ppp.sh.bak
-    sed -i "/proto_run_command/i __username=\$(echo -e \"\$username\")" /lib/netifd/proto/ppp.sh
-    sed -i 's/__username=/\tusername=/' /lib/netifd/proto/ppp.sh
+    /etc/init.d/firewall reload
+    /etc/init.d/netkeeper restart
 }
 
 main() {
     echo "usage: $0 [command]"
     echo "       change_ppp_options"
+    echo "       change_ppp_sh"
     echo "       install_rp_pppoe_so"
     echo "       add_network_netkeeper"
     echo "       set_firewall_for_netkeeper"
     echo "       servers_restart"
-    echo "       enable_rp_pppoe_server"
     exit 0
 }
 
